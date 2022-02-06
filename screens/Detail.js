285159dfe827baa95be740213f77a837c3b065f7
satyamtec2 <satyamtec2@gmail.com>
2022-02-06 16:17:45 +0530

    Movie detail done

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,11 +1,89 @@
-import React from 'react';
-import {Text} from 'react-native';
-const Detail = () => {
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, ScrollView, Image, Dimensions, View} from 'react-native';
+import {getMovie} from '../services/services';
+
+const placeholderImage = require('../assets/images/placeholder.png');
+const height = Dimensions.get('screen').height;
+
+const Detail = ({route, navigation}) => {
+  const movieId = route.params.movieId;
+
+  const [movieDetail, setMovieDetail] = useState();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    getMovie(movieId).then(movieData => {
+      setMovieDetail(movieData);
+      setLoaded(true);
+    });
+  }, [movieId]);
+
   return (
     <React.Fragment>
-      <Text>{'Movie Detail'}</Text>
+      {loaded && (
+        <View>
+          <ScrollView>
+            <Image
+              resizeMode="cover"
+              style={styles.image}
+              source={
+                movieDetail.poster_path
+                  ? {
+                      uri:
+                        'https://image.tmdb.org/t/p/w500' +
+                        movieDetail.poster_path,
+                    }
+                  : placeholderImage
+              }
+            />
+          </ScrollView>
+        </View>
+      )}
     </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: height / 2.5,
+  },
+  movieTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  genresContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  genre: {
+    marginRight: 10,
+    fontWeight: 'bold',
+  },
+  overview: {
+    padding: 15,
+  },
+  release: {
+    fontWeight: 'bold',
+  },
+  playButton: {
+    position: 'absolute',
+    top: -25,
+    right: 20,
+  },
+  videoModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Detail;
--- services/services.js
+++ services/services.js
@@ -35,3 +35,8 @@ export const getDocumentaryMovies = async () => {
   );
   return resp.data.results;
 };
+
+export const getMovie = async id => {
+  const resp = await axios.get(`${apiUrl}/movie/${id}?${apiKey}`);
+  return resp.data;
+};
